63f4d7bb3e94c500e3caad8490057b6af3a7c467
TimeiaVais <93593450+TimeiaVais@users.noreply.github.com>
2022-11-07 09:17:51 +0200

    Lab_4_crud.js edited

--- Lab_4_crud.js
+++ Lab_4_crud.js
@@ -5,33 +5,33 @@ class Car_userCollection {
     get count() {
         return this.items.length;
     }
-    add(user) {
-        if (!(user instanceof Car_user))
-          throw `${user} is not instance of Car_user`;
-        this.items.push(user);
+    add(car_user) {
+        if (!(car_user instanceof Car_user))
+          throw `${car_user} is not instance of Car_user`;
+        this.items.push(car_user);
     }
-    create(userData) {
-        let newcar_user = new Car_user(userData.code, userData.fullname, userData.brand, userData.number, userData.color);
-        this.items.push(newUser);
+    create(car_userData) {
+        let newcar_user = new Car_user(car_userData.code, car_userData.fullname, car_userData.brand, car_userData.number, car_userData.color);
+        this.items.push(newCar_user);
     }
     getById(id) {
-        return this.items.find(user => user.id == id);
+        return this.items.find(car_user => car_user.id == id);
     }
     getAll() {
         return [...this.items];
     }
     update(id, updatedCar_user) {
-        let user = this.getById(id);
-        if (!user)
+        let car_user = this.getById(id);
+        if (!car_user)
           throw `Not found user with id ${id}`;
-        for (let key of user.settablePropertiesList)
+        for (let key of car_user.settablePropertiesList)
         if (updatedCar_user[key])
         user[key] = updatedCar_user[key];
     }
     delete(id) {
-        let userIndex = this.items.findIndex(user => user.id == id);
-        if (userIndex == -1)
-          throw `Not found user with id ${id}`;
-        this.items.splice(userIndex, 1);
+        let car_userIndex = this.items.findIndex(car_user => car_user.id == id);
+        if (car_userIndex == -1)
+          throw `Not found car_user with id ${id}`;
+        this.items.splice(car_userIndex, 1);
     }
 }
